f37300aebb7d8bb6831b9e928980cfa1434aabce
booleanbetrayal <brent.dearth@gmail.com>
2013-12-20 21:57:04 -0500

    updating doc url

--- tasks/release/version.rb
+++ tasks/release/version.rb
@@ -95,7 +95,7 @@ Dir.mktmpdir do |tmpdir|
     f.write(head)
     f.write("#{header}\n\n")
     versions.each do |_version|
-      f.write("[#{_version}](http://www.pedago.com/a-class-above/docs/#{_version})  \n")
+      f.write("[#{_version}](http://pedago.github.io/a-class-above/docs/#{_version})  \n")
     end
   end
   
