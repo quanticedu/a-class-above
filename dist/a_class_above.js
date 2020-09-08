8b698a23655ea67a81a7ffea16bdbfa162c46159
Colby Melvin <colby@pedago.com>
2020-09-08 16:23:59 -0400

    build the dist files

--- dist/a_class_above.js
+++ dist/a_class_above.js
@@ -243,7 +243,10 @@ angular.module('AClassAbove')
      **/
 
     function argumentNames() {
-        var names = this.toString().match(/^[\s\(]*function[^(]*\(([^)]*)\)/)[1]
+        if (!Object.isFunction(this)) {
+            return [];
+        }
+        var names = this.toString().match(/^[\s\(]*[^(]*\(([^)]*)\)/)[1]
             .replace(/\/\/.*?[\r\n]|\/\*(?:.|[\r\n])*?\*\//g, '')
             .replace(/\s+/g, '').split(',');
         return names.length == 1 && !names[0] ? [] : names;
